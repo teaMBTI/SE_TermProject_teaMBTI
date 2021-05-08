d2dad5863043686fd8495632ad43aca13d1b4a20
LetsGit-it <58305490+LetsGit-it@users.noreply.github.com>
2021-05-08 21:31:55 +0900

    push test

--- MBTIMatchingSystem/app/src/main/java/com/seproject/mbtimatchingsystem/WriteInformation.java
+++ MBTIMatchingSystem/app/src/main/java/com/seproject/mbtimatchingsystem/WriteInformation.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+//test push commit
 public class WriteInformation extends AppCompatActivity {
 
     Button studentBtn;

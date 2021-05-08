9058146d260075de20260fa93507b69d2db41415
LetsGit-it <58305490+LetsGit-it@users.noreply.github.com>
2021-05-09 04:10:43 +0900

    push test3

--- MBTIMatchingSystem/app/src/main/java/com/seproject/mbtimatchingsystem/MainActivity.java
+++ MBTIMatchingSystem/app/src/main/java/com/seproject/mbtimatchingsystem/MainActivity.java
@@ -1,5 +1,5 @@
 package com.seproject.mbtimatchingsystem;
-//push test
+//push test3
 import android.content.Intent;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;

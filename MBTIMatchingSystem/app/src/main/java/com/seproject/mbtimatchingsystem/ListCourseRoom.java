4495478715f7e3f7ea0cf4eb12025a2ba022f38e
ekgp3987 <57945707+ekgp3987@users.noreply.github.com>
2021-05-09 23:23:06 +0900

    make listCourseRoom activity(Empty)

---
+++ MBTIMatchingSystem/app/src/main/java/com/seproject/mbtimatchingsystem/ListCourseRoom.java
@@ -0,0 +1,14 @@
+package com.seproject.mbtimatchingsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListCourseRoom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_course_room);
+    }
+}

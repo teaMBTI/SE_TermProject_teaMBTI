2ea09435e8b777c2a9dd0d784ecd6f259d7268c1
Jaeuk Kim <ksyj2006@naver.com>
2021-05-16 19:13:05 +0900

    김재욱 팀 매칭 부분 조금 수정 (아직도 미완료입니다 ㅠ)

--- MBTIMatchingSystem/app/src/main/java/com/seproject/mbtimatchingsystem/MakeTeam.java
+++ MBTIMatchingSystem/app/src/main/java/com/seproject/mbtimatchingsystem/MakeTeam.java
@@ -1,5 +1,6 @@
 package com.seproject.mbtimatchingsystem;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -29,7 +30,7 @@ public class MakeTeam extends AppCompatActivity {
      */
 
     // 처음에 전체 인원을 담을 ArrayList
-    ArrayList<String> mbti = new ArrayList<>();
+    ArrayList<String> student_ID = new ArrayList<>();
 
     // NP/NJ/SP/SJ 별로 분류할 ArrayList
     ArrayList<String> NP = new ArrayList<>();
@@ -54,7 +55,7 @@ public class MakeTeam extends AppCompatActivity {
         setContentView(R.layout.activity_make_team);
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
-        btn = (Button) findViewById(R.id.creatTeam);
+        btn = (Button) findViewById(R.id.createTeam);
 
         mDatabase.child("id_list").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
             @Override
@@ -63,6 +64,9 @@ public class MakeTeam extends AppCompatActivity {
                     Log.e("firebase", "Error getting data", task.getException());
                 }
                 else {
+
+                    // Object를 받아서 학생들의 학번을 ArrayList에 넣는 부분 구현해야함
+
                     Log.d("firebase", String.valueOf(task.getResult().getValue()));
                 }
             }
@@ -75,23 +79,22 @@ public class MakeTeam extends AppCompatActivity {
                  * 1. NP/NJ/SP/SJ 별로 인원 분류 후 팀 매칭
                  * 처음에 모든 인원을 담아놓은 MBTI에서 멤버별로 분류하는 부분
                  */
-                while(mbti.size() != 0) {
-                    String temp = mbti.get(0);
+                while (student_ID.size() != 0) {
+                    String temp = student_ID.get(0);
 
-                    if(temp.substring(3,4).equals("P")) {
-                        if(temp.substring(1,2).equals("N"))
+                    if (temp.substring(3, 4).equals("P")) {
+                        if (temp.substring(1, 2).equals("N"))
                             NP.add(temp);
                         else
                             SP.add(temp);
-                    }
-                    else {
-                        if(temp.substring(1,2).equals("N"))
+                    } else {
+                        if (temp.substring(1, 2).equals("N"))
                             NJ.add(temp);
                         else
                             SJ.add(temp);
                     }
 
-                    mbti.remove(0);
+                    student_ID.remove(0);
                 }
 
 
@@ -103,59 +106,59 @@ public class MakeTeam extends AppCompatActivity {
                  * 2. ArrayList에 담겨있는 멤버수가 minmembers 이상 있는 경우, 앞에서부터 순서대로 minmembers의 수 만큼 뽑아 팀 매칭
                  * 3. 이 과정을 반복하다가, ArrayList 안에 minmembers 보다 적게 남은 경우, 남아있는 멤버들을 J or P 에 맞는 ArrayList에 넣는다
                  */
-                int idx=0;
+                int idx = 0;
 
-                while(NP.size() >= minmembers) {
-                    for(int i=0; i<minmembers; i++) {
+                while (NP.size() >= minmembers) {
+                    for (int i = 0; i < minmembers; i++) {
                         int rand = (int) (Math.random() * NP.size());
                         result[idx][i] = NP.get(rand);
                         NP.remove(rand);
                     }
                     idx++;
-                    if(0 < NP.size() && NP.size() < minmembers) {
-                        while(NP.size() != 0) {
+                    if (0 < NP.size() && NP.size() < minmembers) {
+                        while (NP.size() != 0) {
                             P.add(NP.get(0));
                             NP.remove(0);
                         }
                     }
                 }
 
-                while(NJ.size() >= minmembers) {
-                    for(int i=0; i<minmembers; i++) {
+                while (NJ.size() >= minmembers) {
+                    for (int i = 0; i < minmembers; i++) {
                         result[idx][i] = NJ.get(0);
                         NJ.remove(0);
                     }
                     idx++;
-                    if(0 < NJ.size() && NJ.size() < minmembers) {
-                        while(NJ.size() != 0) {
+                    if (0 < NJ.size() && NJ.size() < minmembers) {
+                        while (NJ.size() != 0) {
                             J.add(NJ.get(0));
                             NJ.remove(0);
                         }
                     }
                 }
 
-                while(SP.size() >= minmembers) {
-                    for(int i=0; i<minmembers; i++) {
+                while (SP.size() >= minmembers) {
+                    for (int i = 0; i < minmembers; i++) {
                         result[idx][i] = SP.get(0);
                         SP.remove(0);
                     }
                     idx++;
-                    if(0 < SP.size() && SP.size() < minmembers) {
-                        while(SP.size() != 0) {
+                    if (0 < SP.size() && SP.size() < minmembers) {
+                        while (SP.size() != 0) {
                             P.add(SP.get(0));
                             SP.remove(0);
                         }
                     }
                 }
 
-                while(SJ.size() >= minmembers) {
-                    for(int i=0; i<minmembers; i++) {
+                while (SJ.size() >= minmembers) {
+                    for (int i = 0; i < minmembers; i++) {
                         result[idx][i] = SJ.get(0);
                         SJ.remove(0);
                     }
                     idx++;
-                    if(0 < SJ.size() && SJ.size() < minmembers) {
-                        while(SJ.size() != 0) {
+                    if (0 < SJ.size() && SJ.size() < minmembers) {
+                        while (SJ.size() != 0) {
                             J.add(SJ.get(0));
                             SJ.remove(0);
                         }
@@ -163,18 +166,18 @@ public class MakeTeam extends AppCompatActivity {
                 }
 
                 // P 재분류
-                while(P.size() >= minmembers) {
-                    for(int i=0; i<minmembers; i++) {
+                while (P.size() >= minmembers) {
+                    for (int i = 0; i < minmembers; i++) {
                         result[idx][i] = P.get(0);
                         P.remove(0);
                     }
                     idx++;
-                    if(0 < P.size() && P.size() < minmembers) {
-                        int tempidx=0;
-                        int added=0;
-                        while(P.size() != 0) {
-                            if(result[tempidx%idx][0].substring(3,4).equals("P")) {
-                                result[tempidx%idx][minmembers+added] = P.get(0);
+                    if (0 < P.size() && P.size() < minmembers) {
+                        int tempidx = 0;
+                        int added = 0;
+                        while (P.size() != 0) {
+                            if (result[tempidx % idx][0].substring(3, 4).equals("P")) {
+                                result[tempidx % idx][minmembers + added] = P.get(0);
                                 P.remove(0);
                             } else {
                                 tempidx++;
@@ -185,18 +188,18 @@ public class MakeTeam extends AppCompatActivity {
 
 
                 // J 재분류
-                while(J.size() >= minmembers) {
-                    for(int i=0; i<minmembers; i++) {
+                while (J.size() >= minmembers) {
+                    for (int i = 0; i < minmembers; i++) {
                         result[idx][i] = J.get(0);
                         J.remove(0);
                     }
                     idx++;
-                    if(0 < J.size() && J.size() < minmembers) {
-                        int tempidx=0;
-                        int added=0;
-                        while(J.size() != 0) {
-                            if(result[tempidx%idx][0].substring(3,4).equals("J")) {
-                                result[tempidx%idx][minmembers+added] = J.get(0);
+                    if (0 < J.size() && J.size() < minmembers) {
+                        int tempidx = 0;
+                        int added = 0;
+                        while (J.size() != 0) {
+                            if (result[tempidx % idx][0].substring(3, 4).equals("J")) {
+                                result[tempidx % idx][minmembers + added] = J.get(0);
                                 J.remove(0);
                             } else {
                                 tempidx++;
@@ -204,7 +207,33 @@ public class MakeTeam extends AppCompatActivity {
                         }
                     }
                 }
+
+
+                // 분류된 팀 매칭 결과를 보여주는 부분 구현해야함
+                // 로직
+                // Result 2d 배열을 번들에 담은 후, 인텐트 실행
+
+
+                Intent intent = new Intent(getApplicationContext(), TeamView.class);
+                Bundle teamlistBundle = new Bundle();
+
+                for(int i=0; i<result.length; i++){
+                    String temp ="";
+                    for(int j=0; j<result[i].length; j++) {
+                        if(result[i][j] != null) {
+                            temp.concat(result[i][j] + " ");
+                        }
+                    }
+
+                    teamlistBundle.putString("Team " + i, temp);
+
+                }
+
+                intent.putExtra("BUNDLE", teamlistBundle);
+                setResult(RESULT_OK,intent);
+
             }
         });
+
     }
 }
---
+++ MBTIMatchingSystem/app/src/main/java/com/seproject/mbtimatchingsystem/TeamView.java
@@ -0,0 +1,17 @@
+package com.seproject.mbtimatchingsystem;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TeamView extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_team_view);
+
+    }
+
+}
